5e3827ec8e54d7c9b409e60af9dcc86348e07ead
Joshua Podhola <joshua.podhola@smoothstack.com>
2021-06-03 19:37:18 -0400

    Authentication now implemented but untested with JWTs.

--- lib_auth/src/main/java/com/ucm/lib/config/BaseSecurityConfig.java
+++ lib_auth/src/main/java/com/ucm/lib/config/BaseSecurityConfig.java
@@ -1,19 +1,36 @@
 package com.ucm.lib.config;
 
+import com.ucm.lib.config.util.AuthEntryPointJwt;
+import com.ucm.lib.config.util.AuthTokenFilter;
 import com.ucm.lib.services.UserInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class BaseSecurityConfig extends WebSecurityConfigurerAdapter {
+    private final UserInfoService userInfoService;
+
+    private final AuthEntryPointJwt unauthorizedHandler;
+
+    /**
+     * Constructor-based injection of dependant components. Not intended to be called.
+     * @author Joshua Podhola
+     * @param userInfoService UserInfoService
+     */
     @Autowired
-    UserInfoService userInfoService;
+    public BaseSecurityConfig(UserInfoService userInfoService, AuthEntryPointJwt unauthorizedHandler) {
+        this.userInfoService = userInfoService;
+        this.unauthorizedHandler = unauthorizedHandler;
+    }
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
@@ -25,6 +42,11 @@ public class BaseSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new CustomPasswordEncoder();
+    }
+
+    @Bean
+    public AuthTokenFilter authenticationJwtTokenFilter() {
+        return new AuthTokenFilter();
     }
 
     /**
@@ -41,5 +63,20 @@ public class BaseSecurityConfig extends WebSecurityConfigurerAdapter {
         public boolean matches(CharSequence charSequence, String s) {
             return charSequence.toString().equals(s);
         }
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable()
+                .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .authorizeRequests()
+                .antMatchers("/auth/**").permitAll()
+                .antMatchers("/api/test/**").permitAll()
+                .antMatchers("/h2-console/**").permitAll()
+                .anyRequest().authenticated();
+
+        http.headers().frameOptions().disable();
+        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 }
