7eb9fe8748c8e71a385ae400cdaa6625c2c4b7d3
Joshua Podhola <joshua.podhola@smoothstack.com>
2021-06-13 18:04:42 -0400

    Enhanced auth library. Bumped version to 0.0.5-SNAPSHOT.

---
+++ lib_auth/src/test/java/com/ucm/lib/services/AuthenticationFacadeTest.java
@@ -0,0 +1,23 @@
+package com.ucm.lib.services;
+
+import com.ucm.lib.BaseTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthenticationFacadeTest extends BaseTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void getAuthenticationTest() {
+    }
+
+    @Test
+    void getUserTest() {
+    }
+}
